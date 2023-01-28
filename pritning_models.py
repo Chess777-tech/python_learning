594f5abdf63515c72ea1cfa2c6d9897ba75c1cf3
Colby Hessifer <120197361+Chess777-tech@users.noreply.github.com>
2023-01-28 11:15:16 -0600

    Add files via upload

---
+++ pritning_models.py
@@ -0,0 +1,7 @@
+import printing_functions as pf
+
+unprinted_design = ['iphone case','robot pendant', 'dodecahedron']
+completed_models = []
+
+pf.print_models(unprinted_design, completed_models)
+pf.show_completed_models(completed_models)
