026bfd858ea63eb3dfa96d7ad8861c9815835490
Colby Hessifer <120197361+Chess777-tech@users.noreply.github.com>
2023-01-27 22:50:01 -0600

    Update 8-12 sandwiches (VSC).py

--- 8-12 sandwiches (VSC).py
+++ 8-12 sandwiches (VSC).py
@@ -1,10 +1,10 @@
-def make_sandwhich(*items):
+def make_sandwich(*items):
     print("\nI'll make you a great sandwhich: ")
     for item in items:
         print(f" ....adding {item} to your sandwhich ")
     print(("Your sandwhich is ready! "))
 
-make_sandwhich('roast beef','cheddar cheese','lettuce','honey dijon')
-make_sandwhich('turkey','apple slices','hoeny mustard')
-make_sandwhich('penut butter','strawberry jam')
+make_sandwich('roast beef','cheddar cheese','lettuce','honey dijon')
+make_sandwich('turkey','apple slices','hoeny mustard')
+make_sandwich('penut butter','strawberry jam')
 
