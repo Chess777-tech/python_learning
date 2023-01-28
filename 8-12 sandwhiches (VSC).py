812f0aca4f13ecdda2243e37bace543e68cc19a2
Colby Hessifer <120197361+Chess777-tech@users.noreply.github.com>
2023-01-27 18:31:49 -0600

    Add files via upload

---
+++ 8-12 sandwhiches (VSC).py
@@ -0,0 +1,10 @@
+def make_sandwhich(*items):
+    print("\nI'll make you a great sandwhich: ")
+    for item in items:
+        print(f" ....adding {item} to your sandwhich ")
+    print(("Your sandwhich is ready! "))
+
+make_sandwhich('roast beef','cheddar cheese','lettuce','honey dijon')
+make_sandwhich('turkey','apple slices','hoeny mustard')
+make_sandwhich('penut butter','strawberry jam')
+
