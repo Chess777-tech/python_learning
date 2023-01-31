a3bf8e11f97f56bae6a19fe79e469fab50e4491e
Colby Hessifer <120197361+Chess777-tech@users.noreply.github.com>
2023-01-31 14:25:07 -0600

    Add files via upload

---
+++ 6-1 favorite numbers (VSC).py
@@ -0,0 +1,36 @@
+"""
+store 5 names and use them as keys...
+store a fav number for each name and use them as values 
+
+
+
+
+"""
+
+
+
+people_fav_numbers = {
+    "Charlie": "7",
+    "Carson":  "8",
+    "colby":   "11",
+    "Cruz":    "10",
+    "Chloe":   "5"
+}
+
+num = people_fav_numbers["Charlie"]
+print(f"Charlies fav number is {num}\n")
+
+
+num = people_fav_numbers["Carson"]
+print(f"Carsons fav number is {num}\n ")
+
+
+num = people_fav_numbers["colby"]
+print(f"Colbys favorite number is {num}\n ")
+
+
+num = people_fav_numbers["Cruz"]
+print(f"Cruzs favorite number is {num}\n ")
+
+num = people_fav_numbers["Chloe"]
+print(f"Chloes favorite number is {num}\n ")
