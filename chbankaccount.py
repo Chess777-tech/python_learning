441727d5a616875477e5f5d39b2634aa8d9e9733
Colby Hessifer <120197361+Chess777-tech@users.noreply.github.com>
2023-02-07 12:47:20 -0600

    Add files via upload

---
+++ chbankaccount.py
@@ -0,0 +1,31 @@
+class BankAccount:
+    def __init__(self, account_holder_name):
+        self.account_holder_name = account_holder_name
+        self._balance = 0
+
+    @property
+    def balance(self):
+        return round(self._balance)
+
+    @balance.setter
+    def balance(self, amount):
+        if not isinstance(amount, (int, float)):
+            pass
+        elif not amount >= 0 or not amount <= 100000:
+            pass
+        else:
+            self._balance = amount
+
+    # balance = property(get_balance, set_balance)  # legacy property
+
+
+def main():
+    account = BankAccount("lone")
+    account.balance = 56.2
+    print(account.balance)
+    account.balance = 56.6
+    print(account.balance)
+
+
+if __name__ == "__main__":
+    main()
