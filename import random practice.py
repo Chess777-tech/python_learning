6585cee3a4eab3cdd0074f722fa5e83c71f49693
Colby Hessifer <120197361+Chess777-tech@users.noreply.github.com>
2023-02-07 11:30:56 -0600

    Add files via upload

---
+++ import random practice.py
@@ -0,0 +1,13 @@
+import random 
+
+greetings = ["hello there!", "Konnichiwa", "Guten tag!", "Bore da! "]
+index = random.randint(0,3)
+print(greetings[index])
+
+
+
+import random 
+
+fruits = ['apple', 'strawberry','bannana', 'blueberry']
+index = random.randint(0,3)
+print(fruits[index])
