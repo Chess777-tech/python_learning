66fb2cd0fca7ab4ef9d2c61742c514debc2eeadd
Colby Hessifer <120197361+Chess777-tech@users.noreply.github.com>
2023-03-18 00:38:59 -0500

    Add files via upload

---
+++ avg_of_list.py
@@ -0,0 +1,11 @@
+def average(numbers):
+    if len(numbers) == 0:
+        return none
+    else:
+        return sum(numbers) / len(numbers)
+
+    
+numbers = [1,2,3,4,5]
+avg = average(numbers)
+print(f"The average of {numbers} is {avg}")
+
